85891470cadf670b61f13ccd14d6f3c7e5e56bbd
munpro2002 <79945464+munpro2002@users.noreply.github.com>
2022-07-07 00:24:49 +0700

    fix seconday navbar

--- src/components/NavBar/SecondaryNav.js
+++ src/components/NavBar/SecondaryNav.js
@@ -1,24 +1,26 @@
+import { Link } from "react-router-dom";
+
 const SecondaryNav = () => {
   return (
     <div className="flex items-center h-[50px] bg-[#F4F4F4] px-4 py-4 ">
       <ul className="max-w-[75rem] flex flex-row w-full items-center font-bold text-[#222222] text-[0.875rem] mx-auto md:pl-[198px] overflow-x-auto no-scrollbar">
         <li className="ml-[8px] md:ml-[0px] whitespace-nowrap mr-[30px] hover:text-[#e40754]">
-          <a href="/">Country</a>
+          <Link to="/home">Country</Link>
         </li>
         <li className="ml-[8px] md:ml-[0px] whitespace-nowrap mr-[30px] hover:text-[#e40754]">
-          <a href="/">User ratings</a>
+          <Link to="/home">User ratings</Link>
         </li>
         <li className="ml-[8px] md:ml-[0px] whitespace-nowrap mr-[30px] hover:text-[#e40754]">
-          <a href="/">Seo tilte</a>
+          <Link to="/home">Seo tilte</Link>
         </li>
         <li className="ml-[8px] md:ml-[0px] whitespace-nowrap mr-[30px] hover:text-[#e40754]">
-          <a href="/">On top</a>
+          <Link to="/home">On top</Link>
         </li>
         <li className="ml-[8px] md:ml-[0px] whitespace-nowrap mr-[30px] hover:text-[#e40754]">
-          <a href="/">Shopable</a>
+          <Link to="/home">Shopable</Link>
         </li>
         <li className="ml-[8px] md:ml-[0px] whitespace-nowrap mr-[30px] hover:text-[#e40754]">
-          <a href="/">Promotion</a>
+          <Link to="/home">Promotion</Link>
         </li>
       </ul>
     </div>
